0008ab539363239205ee86e1df10958689cbe116
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-08-25 01:29:28 +0300

    Update SearchForm.jsx
    Додав ініціалізацію пошукової форми з параметрів запиту, щоб була можливість відкривати стан додатку по посиланню

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,43 +1,102 @@
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import useQueryParams from "../../hooks/useQueryParams";
 
 import css from "./SearchForm.module.css";
 
 const validationSchema = Yup.object({
-  query: Yup.string(),
+  location: Yup.string(),
+  airConditioner: Yup.boolean(),
+  transmission: Yup.boolean(),
+  kitchen: Yup.boolean(),
+  TV: Yup.boolean(),
+  shower: Yup.boolean(),
+  formCamper: Yup.string(),
 });
 
 const SearchForm = () => {
-  const onSearch = (query) => {
-    console.log("Search query:", query);
+  const { getParam, setAllParams } = useQueryParams();
+
+  const getInitialValuesFromParams = () => {
+    return {
+      location: getParam("location") || "",
+      airConditioner: getParam("airConditioner") === "true" || false,
+      transmission: getParam("transmission") === "true" || false,
+      kitchen: getParam("kitchen") === "true" || false,
+      TV: getParam("TV") === "true" || false,
+      shower: getParam("shower") === "true" || false,
+      formCamper: getParam("formCamper") || "",
+    };
+  };
+
+  const onSearch = (values) => {
+    setAllParams(values);
   };
 
   return (
     <Formik
-      initialValues={{ query: "" }}
+      initialValues={getInitialValuesFromParams()}
       validationSchema={validationSchema}
-      onSubmit={(values, { resetForm }) => {
-        resetForm();
+      onSubmit={(values) => {
+        onSearch(values);
       }}
     >
-      {({ values, handleChange }) => (
+      {() => (
         <Form className={css.form} noValidate>
           <div className={css.formGroup}>
-            <label htmlFor="query" className={css.label}>
+            <label htmlFor="location" className={css.label}>
               Location
             </label>
             <Field
               type="text"
-              id="query"
-              name="query"
+              id="location"
+              name="location"
               placeholder="Kyiv, Ukraine"
               className={css.input}
-              onChange={(e) => {
-                handleChange(e);
-                onSearch(e.target.value);
-              }}
             />
           </div>
+
+          <div className={css.checkboxGroup}>
+            <label>
+              <Field type="checkbox" name="airConditioner" />
+              Air Conditioner
+            </label>
+            <label>
+              <Field type="checkbox" name="transmission" />
+              Transmission
+            </label>
+            <label>
+              <Field type="checkbox" name="kitchen" />
+              Kitchen
+            </label>
+            <label>
+              <Field type="checkbox" name="TV" />
+              TV
+            </label>
+            <label>
+              <Field type="checkbox" name="shower" />
+              Shower
+            </label>
+          </div>
+
+          <div className={css.radioGroup}>
+            <label>
+              <Field type="radio" name="formCamper" value="alcove" />
+              Alcove
+            </label>
+            <label>
+              <Field type="radio" name="formCamper" value="fullyIntegrated" />
+              Fully Integrated
+            </label>
+            <label>
+              <Field type="radio" name="formCamper" value="panelTruck" />
+              Panel Truck
+            </label>
+          </div>
+
+          <button type="submit" className={css.submitButton}>
+            Search
+          </button>
         </Form>
       )}
     </Formik>
