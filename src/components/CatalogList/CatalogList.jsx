48786cd60f5bd2eed9c714588014d37fa34c7a6c
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-09-14 18:59:40 +0300

    Adding a message about missing query results

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -6,16 +6,19 @@ import css from "./CatalogList.module.css";
 
 const CatalogList = ({ catalog }) => {
   return (
-    <ul className={css.catalogList}>
-      {catalog &&
-        catalog.map(({ ...item }) => {
-          return (
+    <>
+      {catalog && catalog.length > 0 ? (
+        <ul className={css.catalogList}>
+          {catalog.map((item) => (
             <li className={css.catalogItem} key={nanoid()}>
               <CatalogItem item={item} />
             </li>
-          );
-        })}
-    </ul>
+          ))}
+        </ul>
+      ) : (
+        <p className={css.notFound}>No campers were found for your request</p>
+      )}
+    </>
   );
 };
 
